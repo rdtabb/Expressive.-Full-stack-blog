3e89de09c3a84539d1a96851e2c1d9bef0828001
Rinat Tabaev <rdtabaev@edu.hse.ru>
2023-05-26 19:18:53 +0300

    improved form validation, now user cannot type special symbols, they are sorted out by the regex in the zod schema

--- frontend/src/components/LoginRegister/Login/Login.tsx
+++ frontend/src/components/LoginRegister/Login/Login.tsx
@@ -15,8 +15,23 @@ const Login = () => {
   const { handleLogin } = useLoginRegister();
 
   const registerFormSchema: ZodType<LoginFormData> = z.object({
-    username: z.string().min(2).max(30),
-    password: z.string().min(6).max(20),
+    username: z
+      .string()
+      .trim()
+      .min(2, { message: "Username must be at least 2 characters long" })
+      .regex(/^[A-Za-z0-9]+$/, {
+        message: "Username can contain only letters and numbers",
+      })
+      .max(8, {
+        message: "Username must not be longer than 8 characters",
+      }),
+    password: z
+      .string()
+      .trim()
+      .min(6, {
+        message: "Password must contain at least 6 characters",
+      })
+      .max(20),
   });
 
   const {
--- frontend/src/components/LoginRegister/Register/Register.tsx
+++ frontend/src/components/LoginRegister/Register/Register.tsx
@@ -18,9 +18,32 @@ const Register = () => {
 
   const registerFormSchema: ZodType<RegisterFormData> = z
     .object({
-      username: z.string().min(2).max(30),
-      password: z.string().min(6).max(20),
-      conpassword: z.string().min(6).max(20),
+      username: z
+        .string()
+        .trim()
+        .min(2, {
+          message: "Username must be at least 2 characters long",
+        })
+        .regex(/^[A-Za-z0-9]+$/, {
+          message: "Username can contain only letters and numbers",
+        })
+        .max(8, {
+          message: "Username must not be longer than 8 characters",
+        }),
+      password: z
+        .string()
+        .trim()
+        .min(6, {
+          message: "Password must contain at least 6 characters",
+        })
+        .max(20),
+      conpassword: z
+        .string()
+        .trim()
+        .min(6, {
+          message: "Password must contain at least 6 characters",
+        })
+        .max(20),
     })
     .refine((data) => data.password === data.conpassword, {
       message: "Passwords do not match",
